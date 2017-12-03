2c96338db96fb1b8d1d93a0d9fb077f95e50542e
Anikait Singh <singh.anikait@gmail.com>
2017-12-02 23:36:59 -0500

    Gyro Class functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Gyro.java
@@ -44,75 +44,27 @@ public class Gyro {
         imu.initialize(parameters);
 
         this.telemetry = telemetry;
-        composeTelemetry();
 
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
 
-    }
-
-
-
-
-    void composeTelemetry() {
-
-        telemetry.addAction(new Runnable() { @Override public void run()
-        {
+        new Runnable() {
+            @Override public void run() {
             angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             gravity  = imu.getGravity();
-        }
-        });
-
-        telemetry.addLine()
-                .addData("status", new Func<String>() {
-                    @Override public String value() {
-                        return imu.getSystemStatus().toShortString();
-                    }
-                })
-                .addData("calib", new Func<String>() {
-                    @Override public String value() {
-                        return imu.getCalibrationStatus().toString();
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("heading", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.firstAngle);
-                    }
-                })
-                .addData("roll", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.secondAngle);
-                    }
-                })
-                .addData("pitch", new Func<String>() {
-                    @Override public String value() {
-                        return formatAngle(angles.angleUnit, angles.thirdAngle);
-                    }
-                });
-
-        telemetry.addLine()
-                .addData("grvty", new Func<String>() {
-                    @Override public String value() {
-                        return gravity.toString();
-                    }
-                })
-                .addData("mag", new Func<String>() {
-                    @Override public String value() {
-                        return String.format(Locale.getDefault(), "%.3f",
-                                Math.sqrt(gravity.xAccel*gravity.xAccel
-                                        + gravity.yAccel*gravity.yAccel
-                                        + gravity.zAccel*gravity.zAccel));
-                    }
-                });
+            }
+        }.run();
     }
 
-    String formatAngle(AngleUnit angleUnit, double angle) {
-        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    public double getHeading(){
+        return angles.firstAngle;
     }
 
-    String formatDegrees(double degrees){
-        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    public double getPitch(){
+        return angles.secondAngle;
+    }
+
+    public double getYaw(){
+        return angles.thirdAngle;
     }
 
 }
