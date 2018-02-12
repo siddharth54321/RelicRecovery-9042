47fec8e9a05473d8dd37f26c67155291353b217e
Anikait Singh <singh.anikait@gmail.com>
2018-02-11 21:17:26 -0500

    Fixed Drive Distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.java
@@ -2,14 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 public class RobotMap {
     public static final float GEAR_RATIO = 50;
-    public static final float TICKS_PER_ROTATION = 415;
+    public static final float TICKS_PER_ROTATION = 831;
+    public static final float TICKS_PER_INCH = 110;
     public static final float WHEEL_DIAMETER = 4; //inch
 
     public static final float TURN_TOLERANCE = 6f; //offset due to P only control
     public static final float P_TURN = 0.01f;
 
     public static final float DRIVE_TOLERANCE = 1;
-    public static final double P_CONSTANT_DRIVING = 0.1;
+    public static final float DRIVE_OFFSET = 400;
+    public static final double P_CONSTANT_DRIVING = 0.01;
+
 
     public static final double circumference = Math.PI* WHEEL_DIAMETER;
 
